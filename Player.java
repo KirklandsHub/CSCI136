3eebd8124633f338754e9bed2d76f738b68a135d
KirklandsHub <35820922+KirklandsHub@users.noreply.github.com>
2018-03-10 13:24:15 -1200

    Add files via upload

--- Player.java
+++ Player.java
@@ -7,7 +7,7 @@
 
 public class Player {
 	private static final String IMGNAME = "";	// set as constant because this path shouldn't change
-	private static String playerName;
+	private String playerName;
 	private static int[] position = {0,0};
 	
 	// This class has attributes, but no methods. position and playerName are changed from other
@@ -26,11 +26,8 @@ public class Player {
 	}
 	
 	// playerName getter and setter
-	public static String getPlayerName() {
+	public String getPlayerName() {
 		return playerName;
-	}
-	public static void setPlayerName(String playerName) {
-		Player.playerName = playerName;
 	}
 	
 	// position getter and setter
